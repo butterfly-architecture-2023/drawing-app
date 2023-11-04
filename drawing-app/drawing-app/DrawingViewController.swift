6382665052fc21dfc7073f62784ec72dd7030cbe
piggy.seob <www.piggy.park@groundx.xyz>
2023-11-04 16:27:47 +0900

    init project

---
+++ drawing-app/drawing-app/DrawingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DrawingViewController.swift
+//  drawing-app
+//
+//  Created by 박진섭 on 11/4/23.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+

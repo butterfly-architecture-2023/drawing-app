8eb5067e2b78b93e17b869b38e013be38b1745f4
500beckwon <kjsx60@gmail.com>
2023-11-04 22:42:41 +0900

    project init

---
+++ Drawing/Drawing/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Drawing
+//
+//  Created by ByungHoon Ann on 2023/11/04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

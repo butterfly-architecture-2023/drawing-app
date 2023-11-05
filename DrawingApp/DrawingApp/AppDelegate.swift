af9ba786fb0063adf8ec8e72979dd36edf09da6a
Sanghee <dev.sanghee@gmail.com>
2023-11-05 16:49:10 +0900

    feat: 프로젝트 추가

---
+++ DrawingApp/DrawingApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DrawingApp
+//
+//  Created by 안상희 on 11/5/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+}
---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by 안상희 on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}

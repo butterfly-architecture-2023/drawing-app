26d30f13e9098df460e68b98e693d4f244dec998
Dihee <hwangjihee94@gmail.com>
2023-11-06 22:52:09 +0900

    chore: 프로젝트 초기 설정

---
+++ drawing-app/drawing-app/CanvasViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CanvasViewController.swift
+//  drawing-app
+//
+//  Created by Jihee hwang on 2023/11/06.
+//
+
+import UIKit
+
+final class CanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
+

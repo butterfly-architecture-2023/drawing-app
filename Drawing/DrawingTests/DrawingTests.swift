1effa3c38086ab2f3eaa74596302d8a2b8d4a9c4
500beckwon <kjsx60@gmail.com>
2023-11-08 01:39:24 +0900

    테스트 코드 제작

--- Drawing/DrawingTests/DrawingTests.swift
+++ Drawing/DrawingTests/DrawingTests.swift
@@ -8,29 +8,88 @@
 import XCTest
 @testable import Drawing
 
+func tap(_ button: UIButton) {
+    button.sendActions(for: .touchUpInside)
+}
+
 final class DrawingTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewController: ViewController!
+    
+    override func setUp() {
+        viewController = ViewController()
+        super.setUp()
+        viewController.loadViewIfNeeded()
+        viewController.canvasView.frame = CGRect(origin: .zero, size: CGSize(width: 1000, height: 1000))
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        viewController = nil
+    }
+    
+    func test_그리기_터치시_그리기모드가_활성화되는가() {
+        tap(viewController.makeDrawLineButton)
+        XCTAssertTrue(viewController.isDrawingMode)
+    }
+    
+    // 터치 이벤트를 시뮬레이트하여 그리기 모드가 올바르게 비활성화되는지 확인
+    func test_그리기_재터치시_그리기모드가_비활성화되는가() {
+        viewController.makeDrawLineButton.isSelected = true
+        tap(viewController.makeDrawLineButton)
+        XCTAssertFalse(viewController.isDrawingMode)
+    }
+    
+    func test_사각형버튼_터치시_사각형이_제대로_추가되는가() {
+        tap(viewController.makeRectangleButton)
+        XCTAssertEqual(viewController.canvasView.subviews.count, 1)
+        XCTAssertTrue(viewController.canvasView.subviews.first is RectangleButton)
+    }
+    
+    func test_추가한_사각형을_터치했을때_테두리색이_systemRed인가() {
+        tap(viewController.makeRectangleButton)
+        let rectangle = viewController.getRectangleViews()[0]
+        viewController.handleShapeTap(rectangle)
+        XCTAssertEqual(rectangle.layer.borderColor, UIColor.systemRed.cgColor)
+    }
+    
+    func test_추가한_사각형_재터치시_Border색상이_원래대로_돌아가는가() {
+        tap(viewController.makeRectangleButton)
+        tap(viewController.makeRectangleButton)
+        let rectangle1 = viewController.getRectangleViews()[0]
+        let rectangle2 = viewController.getRectangleViews()[1]
+        viewController.handleShapeTap(rectangle1)
+        viewController.handleShapeTap(rectangle2)
+        XCTAssertNotEqual(rectangle1.layer.borderColor, UIColor.systemRed.cgColor)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_그리기_활성화_할_때_currentDrawingPath가_nil이_아닌가() {
+        tap(viewController.makeDrawLineButton)
+        XCTAssertNotNil(viewController.currentDrawingPath)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_그리기_비활성화_할_때_currentDrawingPath가_nil인가() {
+        tap(viewController.makeDrawLineButton)
+        tap(viewController.makeRectangleButton)
+        XCTAssertNil(viewController.currentDrawingPath)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_그리기버튼이_비활성화시_터치_좌표가_추가되지않는가() {
+        let touch = UITouch()
+        viewController.touchesBegan([touch], with: nil)
+        viewController.touchesMoved([touch], with: nil)
+        XCTAssertNil(viewController.currentDrawingPath)
     }
-
+    
+    // touchesEnded 메서드를 호출하여 그리기 모드에서 그림이 추가되는지 확인
+    func test_그리기를_시도했을_때_그린경로가_Layer에_추가되는가() {
+        tap(viewController.makeDrawLineButton)
+        let touch = UITouch()
+        viewController.touchesMoved([touch], with: nil)
+        viewController.touchesEnded(Set(), with: nil)
+        
+        viewController.touchesMoved([touch], with: nil)
+        viewController.touchesEnded(Set(), with: nil)
+      
+        XCTAssertEqual(viewController.canvasView.layer.sublayers!.count > 1 , true)
+    }
 }

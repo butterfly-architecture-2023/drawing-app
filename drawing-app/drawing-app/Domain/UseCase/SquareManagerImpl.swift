3d5c1383d84e82d47db269d770a254bb63574337
jiyeon <jiyeonlab@gmail.com>
2023-11-09 03:00:13 +0900

    refactor: 네이밍 수정

--- drawing-app/drawing-app/Domain/UseCase/SquareManagerImpl.swift
+++ drawing-app/drawing-app/Domain/UseCase/SquareManagerImpl.swift
@@ -12,16 +12,21 @@ class SquareManagerImpl: SquareManager {
     init() { }
     
     func makeSquare(in rect: CGRect) -> Drawable? {
-        let minX = CGFloat.random(in: rect.origin.x..<rect.width-Constants.squareWidth)
-        let minY = CGFloat.random(in: rect.origin.y..<rect.height-Constants.squareHeight)
+        let minX = CGFloat.random(in: rect.origin.x..<rect.width - Constants.squareWidth)
+        let minY = CGFloat.random(in: rect.origin.y..<rect.height - Constants.squareHeight)
         
-        let points: [CGPoint] = [CGPoint(x: minX, y: minY), CGPoint(x: minX + Constants.squareWidth, y: minY), CGPoint(x: minX + Constants.squareWidth, y: minY + Constants.squareHeight), CGPoint(x: minX, y: minY + Constants.squareHeight)]
+        let points: [CGPoint] = [
+            CGPoint(x: minX, y: minY),
+            CGPoint(x: minX + Constants.squareWidth, y: minY),
+            CGPoint(x: minX + Constants.squareWidth, y: minY + Constants.squareHeight),
+            CGPoint(x: minX, y: minY + Constants.squareHeight)
+        ]
         
         return Square(points: points)
     }
     
     func filteredClick(touchPoint: CGPoint, squares: [Drawable]) -> Drawable? {
-        var selectable: Drawable?
+        var drawable: Drawable?
         squares.forEach { square in
             let x = square.points.map({ $0.x })
             let y = square.points.map({ $0.y })
@@ -32,9 +37,9 @@ class SquareManagerImpl: SquareManager {
             let maxY = y.max() ?? 0
             
             if (minX...maxX).contains(touchPoint.x) && (minY...maxY).contains(touchPoint.y) {
-                selectable = square
+                drawable = square
             }
         }
-        return selectable
+        return drawable
     }
 }

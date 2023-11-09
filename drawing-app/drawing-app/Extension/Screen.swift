0d0d7f2334bddce58da81a55e46c1b8471b152ea
sohee120 <yuhsohee120@gmail.com>
2023-11-09 13:23:51 +0900

    add: 화면 크기 Screen extension

---
+++ drawing-app/drawing-app/Extension/Screen.swift
@@ -0,0 +1,13 @@
+//
+//  Screen.swift
+//  drawing-app
+//
+//  Created by 윤소희 on 2023/11/07.
+//
+
+import UIKit
+
+struct Screen {
+    static let maxWidth = UIScreen.main.bounds.width
+    static let maxHeight = UIScreen.main.bounds.height
+}

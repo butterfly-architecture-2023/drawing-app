a5f9c910878d9be00a7a00af91407d4d25b97dc4
Song-e Han <se.han@dramancompany.com>
2023-11-09 11:06:58 +0900

    test: Rectangle에 대한 unit test 추가

---
+++ SwiftDrawingApp/SwiftDrawingAppTests/RectangleTests.swift
@@ -0,0 +1,30 @@
+//
+//  RectangleTests.swift
+//  SwiftDrawingAppTests
+//
+//  Created by Han Songe on 2023/11/09.
+//
+
+import XCTest
+
+final class RectangleTests: XCTestCase {
+    var rectangle: Rectangle!
+    
+    override func setUpWithError() throws {
+        rectangle = Rectangle(origin: CGPoint(x: 0.0, y: 0.0), size: CGSize(width: 100.0, height: 100.0), fillColor: .systemBlue)
+    }
+    
+    func test_rectangle_frame() {
+        let frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        XCTAssertEqual(rectangle.frame(), frame)
+    }
+    
+    func test_rectangle_addLineToPoints() {
+        let points = rectangle.addLineToPoints()
+        XCTAssertEqual(points, [
+            CGPoint(x: 100.0, y: 0.0),
+            CGPoint(x: 100.0, y: 100.0),
+            CGPoint(x: 0.0, y: 100.0),
+        ])
+    }
+}

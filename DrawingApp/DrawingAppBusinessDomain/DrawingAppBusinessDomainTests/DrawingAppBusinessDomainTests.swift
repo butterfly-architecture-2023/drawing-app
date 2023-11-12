295a952c2d388d650739a26df7e2f8d24ca24c8b
백상휘 <sanghwiback@gmail.com>
2023-11-12 17:27:09 +0900

    Resolve build error in Tests

--- DrawingApp/DrawingAppBusinessDomain/DrawingAppBusinessDomainTests/DrawingAppBusinessDomainTests.swift
+++ DrawingApp/DrawingAppBusinessDomain/DrawingAppBusinessDomainTests/DrawingAppBusinessDomainTests.swift
@@ -17,14 +17,14 @@ final class DrawingAppBusinessDomainTests: XCTestCase {
   func test_매니저_엔티티_생성_테스트() throws {
     for _ in 0..<3 {
       let _ = manager.addSquare()
-      let _ = manager.addVector(from: .init(x: 0, y: 0))
+      let _ = manager.addVector(data: nil)
     }
     
     XCTAssertEqual(manager.squares.count, manager.vectors.count)
   }
   
   func test_매니저_사각형_엔티티_랜덤_위치_테스트() throws {
-    var position: GesturePosition?
+    var position: CanvasPosition?
     for _ in 0..<3 {
       let square = manager.addSquare()
       
@@ -35,16 +35,16 @@ final class DrawingAppBusinessDomainTests: XCTestCase {
   
   func test_사각형_엔티티_move() throws {
     let square = manager.addSquare()
-    let mocks: [GesturePosition] = [
-      GesturePosition(x: 0, y: 0),
-      GesturePosition(x: 100, y: 100),
-      GesturePosition(x: 200, y: 200),
-      GesturePosition(x: 300, y: 300),
-      GesturePosition(x: 400, y: 400),
+    let mocks: [CanvasPosition] = [
+      CanvasPosition(x: 0, y: 0),
+      CanvasPosition(x: 100, y: 100),
+      CanvasPosition(x: 200, y: 200),
+      CanvasPosition(x: 300, y: 300),
+      CanvasPosition(x: 400, y: 400),
     ]
     
     for mock in mocks {
-      let result = manager.moveSquare(square, at: mock)
+      let result = manager.move(square: square, at: mock)
       XCTAssertTrue(result)
       XCTAssertEqual(square.position, mock)
     }
